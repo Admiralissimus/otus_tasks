0d78a6492f5b8a132e9eab9863e1e1a3e8aedc51
Aliaksandr Ushakou <aushakou@gmail.com>
2023-11-07 20:14:24 +0300

    7_yc fix some bugs in puma deploy

--- 07_YC_Packer_immutable_baked/scripts/install_puma.sh
+++ 07_YC_Packer_immutable_baked/scripts/install_puma.sh
@@ -4,7 +4,10 @@ apt-get update
 apt-get install -y git
 useradd -m -U -d /var/puma puma
 cd /var/puma
-git clone -b monolith https://github.com/admiralissimus/reddit.git
-cd reddit && bundle install
+sudo -u puma git clone -b monolith https://github.com/admiralissimus/reddit.git
+cd reddit 
+sudo -u puma bundle install
+mv /tmp/puma.service /etc/systemd/system/puma.service 
+chown root:root /etc/systemd/system/puma.service
 systemctl start puma
 systemctl enable puma
