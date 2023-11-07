dec682d385330c89b21cd826e4e9a8a86d17acba
Aliaksandr Ushakou <aushakou@gmail.com>
2023-11-07 21:12:03 +0300

    07_yc fix bug in install_puma.sh

--- 07_YC_Packer_immutable_baked/scripts/install_puma.sh
+++ 07_YC_Packer_immutable_baked/scripts/install_puma.sh
@@ -4,9 +4,10 @@ apt-get update
 apt-get install -y git
 useradd -m -U -d /var/puma puma
 cd /var/puma
-sudo -u puma git clone -b monolith https://github.com/admiralissimus/reddit.git
+git clone -b monolith https://github.com/admiralissimus/reddit.git
 cd reddit 
-sudo -u puma bundle install
+bundle install
+chown -R puma:puma /var/puma
 mv /tmp/puma.service /etc/systemd/system/puma.service 
 chown root:root /etc/systemd/system/puma.service
 systemctl start puma
